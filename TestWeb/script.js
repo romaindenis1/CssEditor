66da3799968fed85870630e50e4164d434eb275b
MBAMER <163824331+MBAMER@users.noreply.github.com>
2024-11-05 10:11:29 +0100

    Amélioration du code

--- TestWeb/script.js
+++ TestWeb/script.js
@@ -4,6 +4,7 @@ document.getElementById('apply-styles').addEventListener('click', function() {
     let backgroundColor = document.getElementById('background-color').value;
     let textColor = document.getElementById('text-color').value;
     let fontSize = document.getElementById('font-size').value;
+    let fontFamily = document.getElementById('font-family').value;
     let outputText = document.getElementById('output-text');
     let cssCode = document.getElementById('css-code');
 
@@ -27,6 +28,11 @@ document.getElementById('apply-styles').addEventListener('click', function() {
     if (fontSize) {
         outputText.style.fontSize = fontSize;
         cssText += `font-size: ${fontSize};\n`;
+    }
+
+    if (fontFamily) {
+        outputText.style.fontFamily = fontFamily;
+        cssText += `font-family: ${fontFamily};\n`;
     }
 
     cssCode.textContent = `#output-text {\n${cssText}}`;
